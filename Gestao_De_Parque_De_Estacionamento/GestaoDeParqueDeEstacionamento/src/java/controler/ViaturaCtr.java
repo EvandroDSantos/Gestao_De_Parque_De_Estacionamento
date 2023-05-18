48f4cd049273414a31ea76695513e1ae353cedea
UmeidIBraimo <umeidibraimo11@gmail.com>
2023-05-18 20:32:18 +0200

    Update ViaturaCtr.java

--- Gestao_De_Parque_De_Estacionamento/GestaoDeParqueDeEstacionamento/src/java/controler/ViaturaCtr.java
+++ Gestao_De_Parque_De_Estacionamento/GestaoDeParqueDeEstacionamento/src/java/controler/ViaturaCtr.java
@@ -5,10 +5,68 @@
  */
 package controler;
 
+import models.Viatura;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  *
  * @author Dos SanTos
  */
 public class ViaturaCtr {
+    public void Inserir(Viatura e){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session sessao = sessionFactory.openSession();
+        Transaction tr = null;
+        
+        try {
+            tr = sessao.beginTransaction();
+            sessao.saveOrUpdate(e);
+
+            tr.commit();
+        } catch (Exception el) {
+            el.printStackTrace();
+            tr.rollback();
+        }
+        
+    }
+    
+    
+    public void delete(Viatura e){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session sessao = sessionFactory.openSession();
+        Transaction tr = null;
+        
+        try {
+            tr = sessao.beginTransaction();
+            sessao.delete(e);
+
+            tr.commit();
+
+        } catch (Exception el) {
+            el.printStackTrace();
+            tr.rollback();
+        }  
+    }
+    
+    
+    public void update(Viatura e){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session sessao = sessionFactory.openSession();
+        Transaction tr = null;
+        
+        try {
+            tr = sessao.beginTransaction();
+            sessao.update(e);
+
+            tr.commit();
+
+        } catch (Exception el) {
+            el.printStackTrace();
+            tr.rollback();
+        }  
+    }
     
 }
