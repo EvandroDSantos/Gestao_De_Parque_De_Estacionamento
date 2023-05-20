5ad8f62601003b857620803a70be904f409d1c8c
Abdul Arnaldo <abdularnaldo@gmail.com>
2023-05-20 14:59:32 +0200

    Update ClienteCtr.java

--- Gestao_De_Parque_De_Estacionamento/GestaoDeParqueDeEstacionamento/src/java/controler/ClienteCtr.java
+++ Gestao_De_Parque_De_Estacionamento/GestaoDeParqueDeEstacionamento/src/java/controler/ClienteCtr.java
@@ -5,10 +5,68 @@
  */
 package controler;
 
+import models.Cliente;
+import models.Viatura;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  *
  * @author Dos SanTos
  */
 public class ClienteCtr {
+    public void Inserir(Cliente c){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session sessao = sessionFactory.openSession();
+        Transaction tr = null;
+        
+        try {
+            tr = sessao.beginTransaction();
+            sessao.saveOrUpdate(c);
+
+            tr.commit();
+        } catch (Exception el) {
+            el.printStackTrace();
+            tr.rollback();
+        }
+        
+    }
     
+    
+    public void delete(Cliente c){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session sessao = sessionFactory.openSession();
+        Transaction tr = null;
+        
+        try {
+            tr = sessao.beginTransaction();
+            sessao.delete(c);
+
+            tr.commit();
+
+        } catch (Exception el) {
+            el.printStackTrace();
+            tr.rollback();
+        }  
+    }
+    
+    
+    public void update(Cliente c){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session sessao = sessionFactory.openSession();
+        Transaction tr = null;
+        
+        try {
+            tr = sessao.beginTransaction();
+            sessao.update(c);
+
+            tr.commit();
+
+        } catch (Exception el) {
+            el.printStackTrace();
+            tr.rollback();
+        }  
+    }
 }
